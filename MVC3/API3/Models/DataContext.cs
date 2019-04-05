c6ae37c80bea46a72c5a3f82f4abe8a521d268da
MAURO ALEJANDRO HERNANDEZ DE LAS MUÑECAS <mauroahdlm@gmail.com>
2019-04-04 23:54:44 -0400

    commit con modelo web api

---
+++ MVC3/API3/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace API3.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<API3.Models.Product> Products { get; set; }
+    }
+}
